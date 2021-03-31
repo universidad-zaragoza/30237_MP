8b777ce6520ca6ad23d599d5d891709d3295dd5a
RubÃen Gran Tejero <rgran@unizar.es>
2021-03-31 16:25:48 +0200

    Anadido ejercicios 2 b y 2 c de la parte 2 de la sesion.

---
+++ p4/parte2/ejer2c.cpp
@@ -0,0 +1,26 @@
+/* ejer2c.cpp */
+#include <omp.h>
+#include <iostream>
+#include <random>
+
+const unsigned int N = 1000000;
+double A[N];
+
+int main(void){
+  int nthreads, tnumber;
+
+  std::random_device rd;  // Se utilizará para sembrar el generador de aleatorios
+  std::mt19937 gen(rd()); // Sembrado de  mersenne_twister_engine con rd()
+  std::uniform_real_distribution<> dis(0.0, 100.0); //Configuración del espacio de de generación
+
+  for (int i = 0; i < N; ++i) {
+    A[i] = dis(gen);
+  }
+  
+  for (int i = 1; i < N; ++i) {
+    A[i] = A[i] - A[i-1];
+  }
+  
+  std::cout << "A[150]=" << A[150] << std::endl;
+
+}
