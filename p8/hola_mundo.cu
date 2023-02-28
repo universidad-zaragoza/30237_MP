6fb1b5255c62df1508917160cde92bc3992b4e36
RubÃen Gran Tejero <rgran@unizar.es>
2023-02-28 19:29:34 +0100

    Fichero para el primer punto de la sesiÃn de prÃcticas, hola_mundo.cu

---
+++ p8/hola_mundo.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void cuda_hello(){
+    printf("Hello World from GPU!\n");
+}
+
+int main() {
+   
+    cuda_hello<<<1,1>>>(); 
+    
+	cudaError_t cudaerr = cudaDeviceSynchronize();
+    if (cudaerr != cudaSuccess)
+        printf("kernel launch failed with error \"%s\".\n",
+               cudaGetErrorString(cudaerr));
+
+	return 0;
+}
+
