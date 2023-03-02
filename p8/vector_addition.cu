2be2fbfcf5b50cbbcfa7ddb837c691db23e9aacf
RubÃen Gran Tejero <rgran@unizar.es>
2023-03-02 18:00:08 +0100

    Primera version de la transformacion de la suma de vectores
    ´# Please enter the commit message for your changes. Lines starting

---
+++ p8/vector_addition.cu
@@ -0,0 +1,30 @@
+#define N 10000000000
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void vector_add(float *out, float *a, float *b, long int n) {
+    for(long int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out; 
+
+    printf("%d\n", sizeof(long int));
+
+    // Allocate memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(long int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    // Main function
+    vector_add(out, a, b, N);
+}
+
