fa5ada59fee42ed1d3477a447b49267b23c9da42
Rubén Gran Tejero <rgran@unizar.es>
2026-04-12 23:08:23 +0200

    Remove dynamic setting for OpenMP threads
    Removed dynamic setting for OpenMP threads.

--- 2_paralelos/p4/parte1/nested.cpp
+++ 2_paralelos/p4/parte1/nested.cpp
@@ -6,7 +6,6 @@ int main(void){
   int i;
   int nthreads, tnumber;
 
-  omp_set_dynamic(????);  // Ver el manual 
   //omp_set_max_active_levels(1);  // Sin anidamiento
   //omp_set_max_active_levels(5);  // Hasta 5 niveles de anidamiento
   omp_set_num_threads(2);
@@ -14,13 +13,13 @@ int main(void){
   #pragma omp parallel private(nthreads, tnumber)
   {
     tnumber = omp_get_thread_num();
-    std::cout << "Primera región paralela: thread " + std::to_string(tnumber) + " de " + std::to_string(omp_get_num_threads()) << std::endl;
+    std::cout << "Primera regiÃ³n paralela: thread " + std::to_string(tnumber) + " de " + std::to_string(omp_get_num_threads()) << std::endl;
 
     omp_set_num_threads(4);
 
     #pragma omp parallel firstprivate(tnumber)
     {
-      std::cout << "Región anidada paralela (Equipo " + std::to_string(tnumber) + "): thread " + std::to_string(omp_get_thread_num()) + " de " + std::to_string(omp_get_num_threads()) << std::endl;
+      std::cout << "RegiÃ³n anidada paralela (Equipo " + std::to_string(tnumber) + "): thread " + std::to_string(omp_get_thread_num()) + " de " + std::to_string(omp_get_num_threads()) << std::endl;
     }
   }
   return 0;
