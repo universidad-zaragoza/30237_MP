65034e9d3838441fd2c0f905bb70edd2037ac784
RubÃen Gran Tejero <rgran@unizar.es>
2026-04-19 09:30:20 +0200

    new file exemplifying the use of openMP tasks

---
+++ 2_paralelos/p5/matmul/task.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <omp.h>
+
+void trabajo(int id) {
+
+   std::cout << "Inicio tarea " << id << " en thread " << omp_get_thread_num() << std::endl;
+        
+   // Simular trabajo
+   for (volatile int i = 0; i < 100000000; i++);
+   
+   std::cout << "Fin tarea " << id << " en thread " << omp_get_thread_num() << std::endl;
+}
+ 
+int main() {
+    #pragma omp parallel
+    {
+    #pragma omp single   // solo un thread crea las tareas
+    {
+       std::cout << "Creando tareas...\n";
+
+       #pragma omp task
+       trabajo(1);
+
+       #pragma omp task
+       trabajo(2);
+
+       #pragma omp task
+       trabajo(3);
+
+       // Esperar a que todas las tareas terminen
+       #pragma omp taskwait
+
+    std::cout << "Todas las tareas han finalizado\n";
+        }
+        }
+                         return 0;
+                                                                                                                                                                                 }
