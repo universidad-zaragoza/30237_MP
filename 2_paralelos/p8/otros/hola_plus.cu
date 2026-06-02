266bc98916143106100e5fe342f916515525198a
RubÃen Gran Tejero <rgran@unizar.es>
2026-06-02 23:28:12 +0200

    blockDim and gridDim

--- 2_paralelos/p8/otros/hola_plus.cu
+++ 2_paralelos/p8/otros/hola_plus.cu
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 __global__ void hello() {
-    printf("Hola desde la GPU! bloque %d thread %d\n", blockIdx.x, threadIdx.x);
+    //Pregunta: ¿que significan los siguiente 4 valores
+    printf("Hola desde la GPU! bloque %d thread %d gridDim %d blockDim %d\n", blockIdx.x, threadIdx.x, gridDim.x, blockDim.x);
 }
 
 int main() {
